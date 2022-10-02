71a38a6a86d2f62e5073efaec20ab3370682847b
v <veiakurman@gmail.com>
2022-10-01 19:24:10 -0500

    Complete 02_repeatString Exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
+const repeatString = function(userInput, repeatNum) {
+    let string = "";
 
+    if (repeatNum < 0) return "ERROR";
+
+    for (let i = 0; i < repeatNum; i++) {
+        string += userInput;
+    }
+
+    return string;
 };
 
 // Do not edit below this line
