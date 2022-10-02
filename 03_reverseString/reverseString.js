9f5ce87b42e0825aa58943f271208a28e6f0974f
v <veiakurman@gmail.com>
2022-10-01 20:07:25 -0500

    Complete Exercise 03_reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    let userInput = string.toString();
+    let userInputSplit = userInput.split("");
+    let userInputReversed = userInputSplit.reverse();
+    let userInputJoined = userInputReversed.join("");
 
+    return userInputJoined;
 };
 
 // Do not edit below this line
