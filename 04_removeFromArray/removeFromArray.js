c28a051c2a4ae85936dcac8c61b91c22f470682c
v <veiakurman@gmail.com>
2022-10-02 12:50:35 -0500

    Partially Complete 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,8 @@
-const removeFromArray = function() {
-
+const removeFromArray = function (array, num) {
+    const userArray = array;
+    let userNum = num - 1;
+    userArray.splice(userNum, 1);
+    return userArray;
 };
 
 // Do not edit below this line
