3ff2ead953c4ba22ab79ce70394fa3866616cb02
Veia Kurman <veiakurman@gmail.com>
2022-10-04 05:27:58 -0500

    For 04_removeFromArray, Refactor Code
    Also added pseudocode for upcoming changes in file.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,9 +1,19 @@
-const removeFromArray = function (array, num) {
-    const userArray = array;
-    let userNum = num - 1;
-    userArray.splice(userNum, 1);
-    return userArray;
+const removeFromArray = function (array, num, ...moreArgs) {
+    array.splice(num - 1, 1);
+    return array;
 };
+
+/*
+if typeof `num` == number &&  num - 1 > `array.length`:
+    continue;
+if typeof `num` == string && `array` does not include `num`:
+    continue;
+while looping through values in `moreArgs`:
+    if typeof `moreArgs[?]` == number &&  `moreArgs[?]` - 1 > `array.length`:
+        continue;
+    if typeof `moreArgs[?]` == string && `array` does not include `moreArgs[?]`:
+        continue;
+*/
 
 // Do not edit below this line
 module.exports = removeFromArray;
