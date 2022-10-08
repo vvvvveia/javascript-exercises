6f203a628b44c66b53c46bbe07cccc0e1cc6339b
Veia Kurman <veiakurman@gmail.com>
2022-10-08 13:41:49 -0500

    Solve 04_removeFromArray, Update Project

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,27 +1,13 @@
-const removeFromArray = function (array, input, ...moreInput) {
-    if (typeof input === "number" && input - 1 <= array.length) {
-        array.splice(input - 1, 1);
-    } else if (typeof input === "string" && array.includes(input)) {
-        array.splice(array.indexOf(input), 1);
-    }
+const removeFromArray = function (array, ...input) {
+    const newArray = [];
 
-    if (moreInput.length >= 0) {
-        for (let inputData of moreInput) {
-            if (
-                typeof inputData === "number" &&
-                inputData <= array.length
-            ) {
-                array.splice(inputData - 1, 1);
-            } else if (
-                typeof inputData === "string" &&
-                array.includes(inputData)
-            ) {
-                array.splice(array.indexOf(inputData), 1);
-            }
+    array.forEach((item) => {
+        if (!input.includes(item)) {
+            newArray.push(item);
         }
-    }
+    });
 
-    return array;
+    return newArray;
 };
 
 // Do not edit below this line
