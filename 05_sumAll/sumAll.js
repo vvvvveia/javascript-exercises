35f137b0fed0853e76bdbf19d4d6badbf08017a2
Veia Kurman <veiakurman@gmail.com>
2022-10-08 16:21:26 -0500

    Create Basic Structure of 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,54 @@
-const sumAll = function() {
+const sumAll = function (num1, num2) {
+    const numsToAdd = [];
+    let total = 0;
 
+    if (
+        num1 > num2 &&
+        num1 >= 0 &&
+        num2 >= 0 &&
+        typeof num1 === "number" &&
+        typeof num2 === "number"
+    ) {
+        for (let i = num2; i < num1; i++) {
+            numsToAdd.push(i);
+            for (let num of numsToAdd) {
+                total += num;
+                console.log(total);
+            }
+        }
+    } else if (
+        num2 > num1 &&
+        num1 >= 0 &&
+        num2 >= 0 &&
+        typeof num1 === "number" &&
+        typeof num2 === "number"
+    ) {
+        for (let i = num1; i < num2; i++) {
+            numsToAdd.push(i);
+            for (let num of numsToAdd) {
+                total += num;
+                console.log(total);
+            }
+        }
+    } else {
+        return "ERROR";
+    }
+
+    return total;
 };
 
+/*
+if num1 > num2 && num1 and num2 > 0:
+    while num1 > num2, increment num1:
+        add each number to numsToAdd array then += total;
+        return total;
+else if num2 > num1 && num1 and num2 > 0:
+    while num2 > num1, increment num1:
+        add each number to numsToAdd array then += total;
+        return total;
+else
+    return "ERROR";
+ */
+
 // Do not edit below this line
-module.exports = sumAll;
+// module.exports = sumAll;
