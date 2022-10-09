68cac686b476c77cbc4008c16cdd7edee0b4fe7c
Veia Kurman <veiakurman@gmail.com>
2022-10-08 19:38:09 -0500

    Create Format of 06_leapYears, Add Pseudocode

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,19 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    if (/* conditionals */) {
+        return true;
+    } else {
+        return false;
+    }
 };
+
+/*
+if year:
+    is divisible by 4
+    is not divisible by 100 unless divisible by 400
+        return true
+else:
+    return false
+ */
 
 // Do not edit below this line
 module.exports = leapYears;
