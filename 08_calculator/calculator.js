9f0af97256d75faeb91617f228cb726c571baa78
Veia Kurman <veiakurman@gmail.com>
2022-10-08 21:10:36 -0500

    Solve add of 08_calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,13 +1,19 @@
-const add = function() {
-	
+const add = function(...numsToAdd) {
+    let result = 0;
+
+    for (let num of numsToAdd) {
+        result += num;
+    }
+
+    return result;
 };
 
 const subtract = function() {
-	
+
 };
 
 const sum = function() {
-	
+
 };
 
 const multiply = function() {
@@ -15,19 +21,19 @@ const multiply = function() {
 };
 
 const power = function() {
-	
+
 };
 
 const factorial = function() {
-	
+
 };
 
 // Do not edit below this line
 module.exports = {
-  add,
-  subtract,
-  sum,
-  multiply,
-  power,
-  factorial
+    add,
+    subtract,
+    sum,
+    multiply,
+    power,
+    factorial
 };
