7d151f0e37ad5a73db1c25968c55b4f166fe399d
Veia Kurman <veiakurman@gmail.com>
2022-10-08 20:34:33 -0500

    Solve 07_tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,13 +1,15 @@
-const ftoc = function() {
-
+const ftoc = function (f) {
+    let newF = ((f - 32) * 5) / 9;
+    return Math.round(newF * 10) / 10;
 };
 
-const ctof = function() {
-
+const ctof = function (c) {
+    let newC = c * 1.8 + 32;
+    return Math.round(newC * 10) / 10;
 };
 
 // Do not edit below this line
 module.exports = {
-  ftoc,
-  ctof
+    ftoc,
+    ctof,
 };
