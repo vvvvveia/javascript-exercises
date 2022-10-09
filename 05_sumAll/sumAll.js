e6e7eb52ebc389dd739bc6d50be84fdd91d9bb96
Veia Kurman <veiakurman@gmail.com>
2022-10-08 19:05:38 -0500

    Solve 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,45 +1,27 @@
-const sumAll = function (num1, num2) {
+const sumAll = function (min, max) {
     let total = 0;
 
     if (
-        num1 > num2 &&
-        num1 >= 0 &&
-        num2 >= 0 &&
-        typeof num1 === "number" &&
-        typeof num2 === "number"
+        typeof min !== "number" ||
+        typeof max !== "number" ||
+        min < 0 ||
+        max < 0
     ) {
-        for (let i = num2; i < num1; i++) {
-            total += 1;
-        }
-    } else if (
-        num2 > num1 &&
-        num1 >= 0 &&
-        num2 >= 0 &&
-        typeof num1 === "number" &&
-        typeof num2 === "number"
-    ) {
-        for (let i = num1; i < num2; i++) {
-            total += 1;
-        }
-    } else {
         return "ERROR";
+    }
+    if (min > max) {
+        const temp = min;
+        min = max;
+        max = temp;
+    }
+    for (let i = min; i < max + 1; i++) {
+        total += i;
     }
 
     return total;
 };
 
-/*
-if num1 > num2 && num1 and num2 > 0:
-    while num1 > num2, increment num1:
-        add each number to numsToAdd array then += total;
-        return total;
-else if num2 > num1 && num1 and num2 > 0:
-    while num2 > num1, increment num1:
-        add each number to numsToAdd array then += total;
-        return total;
-else
-    return "ERROR";
- */
+module.exports = sumAll;
 
 // Do not edit below this line
 module.exports = sumAll;
